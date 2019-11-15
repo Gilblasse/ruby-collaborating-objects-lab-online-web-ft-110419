ea77fb6b1c901f7c82602c5808355a7059dfb05e
Nethelbert <gilblasse@gmail.com>
2019-11-15 15:49:53 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -12,10 +12,11 @@ class Song
   end
   
   def self.new_by_filename(file_name)
-    artist, title, genre = file_name[21..-5].split('-')
-    song = Song.new(title.strip)
-    song.genre = genre.strip
-    song.artist_name = artist.strip
+    
+ 
+    song = Song.new()
+    song.genre = 
+    song.artist_name =
     song
   end
   
