7e281fb1cfa61a7305d727f8ece205ac5fd9b6b6
Nethelbert <gilblasse@gmail.com>
2019-11-15 16:40:08 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -8,7 +8,7 @@ class MP3Importer
   end
   
   def files
-   files = Dir["#{@path}/*.mp3"].collect {|f| Song.new_by_filename(f[21..-1])}
+   Dir["#{@path}/*.mp3"].collect {|f| Song.new_by_filename(f[21..-1])}
   # binding.pry
   end
   
